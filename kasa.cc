dae80909a4bea1a324b7b189405848dfe2502289
Cezary Bednarz <cb406099@dyn-10-0-8-164.mimuw.edu.pl>
2019-10-09 15:46:20 +0200

    basic project template

---
+++ kasa.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * tab to 4 spacje xDDD
+ */
+
+
+// zaimplementuj te 3 funkcje xdddddddddd
+bool checkBusRouteCommand(string command) {
+
+}
+
+bool checkNewTicketCommand(string command) {
+
+}
+
+bool checkTicketRequestCommand(string command) {
+
+}
+
+
+
+
+void input() {
+
+    int lineId = 1;
+    string line;
+     
+    while(getline(cin, line)) {
+    
+        cout << "linijka " << lineId << ": " << line;
+        lineId++;
+    }
+}
+
+int main() {
+
+    input();    
+    return 0;
+}
