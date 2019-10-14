dac0144c6765bb9bce73df44cb33315cbec28d10
Cezary Bednarz <cb406099@dyn-10-0-8-164.mimuw.edu.pl>
2019-10-14 13:51:40 +0200

    almost all, need to add output

--- kasa.cc
+++ kasa.cc
@@ -7,8 +7,10 @@
 #include <cstdio>
 #include <cstdlib>
 #include <map>
+#include <climits>
 
 using ULL = unsigned long long;
+using INF = 1e18 + 10;
 
 const std::regex reg1("\\d+(\\s(5:5[5-9]|([6-9]|1[0-9]|20):\\d\\d|21:1[0-9]|21:2[0-1])\\s([a-zA-Z]|\\^)([a-zA-Z]|\\s|\\^)*)+");
 const std::regex reg2("[a-zA-Z]([a-zA-Z]|\\s)*\\s([1-9]\\d*|0)\\.\\d\\d\\s[1-9][0-9]*");
@@ -43,6 +45,129 @@ std::tuple<std::string, ULL, int> parseNewTicketCommand(std::string command) {
 std::pair<std::vector<std::string>, std::vector<std::string>> parseTicketRequestCommand(std::string command) {
 
 }
+
+
+
+// nie mozna zmiennych globalnych
+// using zamiast pisania poteznych typow
+
+std::map<std::string, std::map<int, std::string>> busRoutes;
+std::map<std::string, std::map<std::string, int>> busStops;
+std::vector<std::tuple<std::string, ULL, int>> tickets;
+
+void executeBusRoute(std::pair<std::string, std::vector<std::pair<int, std::string>>> busRoute) {
+    auto name = busRoute.first;
+    auto timetable = busRoute.second;
+    
+    for(auto p : timetable) {
+        auto time = p.first;
+        auto busStop = p.second;
+        
+        busRoutes[name][time] = busStop;
+        busStops[busStop][name] = time;
+    }
+}
+
+void executeNewTicket(std::tuple<std::string, ULL, int> ticket) {
+    tickets.push_back(cmd);
+}
+
+ULL checkTickets(std::vector<std::tuple<std::string, ULL, int> toCheck, int time) {
+    int sumTime = 0;
+    ULL sumPrice = 0LL;
+    
+    for(auto t : toCheck) {
+        sumTime += std::get<2>(t);
+        sumPrice += std::get<1>(t);
+    }
+    
+    if(sumTime < time) {
+        return INF;
+    }
+    
+    return sumPrice;
+}
+
+bool executeTicketRequest(std::pair<std::vector<std::string>, std::vector<std::string>> busRide) {
+    int time = 0;
+    auto stops = busRide.first;
+    auto routes = busRide.second;
+    int prevTimestamp = busStops[stops[0]][routes[0]];
+    
+    for(int i = 0; i < stops.size(); i++) {
+        auto currStop = stops[i];
+        auto prevRoute = routes[i];
+        auto nextRoute = routes[i + 1];
+        
+        int prevTime = busStops[currStop][prevRoute];
+        int nextTime = busStops[currStop][nextRoute];
+        
+        if(prevTime != nextTime) {
+            // wypisac nazwe przystanku
+            return false;
+        }
+        
+        time += prevTime - prevTimestamp + 1;
+        prevTimestamp = prevTime;
+    }
+    
+    
+    std::vector<std::string>> bestTickets;
+    ULL lowestPrice = INF;
+    
+    if(tickets.size() >= 1) {    
+        for(int i = 0; i < tickets.size(); i++) {
+        
+            auto currScore = checkTickets({tickets[i], time});
+            if(currScore.first && lowestPric) {  
+                
+            }
+        }
+    } 
+    else if(tickets.size() >= 2) {
+        for(int i = 0; i < tickets.size(); i++) {
+            for(int j = i; j < tickets.size(); j++) {
+            
+                if(std::get<2>(tickets[i]) + std::get<2>(tickets[j]) >= time) {
+                    ULL currPrice = std::get<1>(tickets[i]) + std::get<1>(tickets[j]);
+                    
+                    if(currPrice < lowestPrice) {
+                        lowestPrice = currPrice;
+                        bestTickets = {std::get<0>(tickets[i]), std::get<0>(tickets[j])};
+                    }
+                }
+            }
+        }
+    } 
+    else if(tickets.size() >= 3) {
+        for(int i = 0; i < tickets.size(); i++) {
+            for(int j = i; j < tickets.size(); j++) {
+                for(int k = j; k < tickets.size(); k++) {
+                    
+                    if(std::get<2>(tickets[i]) + std::get<2>(tickets[j]) + std::get<2>(tickets[k]) >= time) {
+                        ULL currPrice = std::get<1>(tickets[i]) + std::get<1>(tickets[j]) + std::get<1>(tickets[k]);
+                        
+                        if(currPrice < lowestPrice) {
+                            lowestPrice = currPrice;
+                            bestTickets = {std::get<0>(tickets[i]), std::get<0>(tickets[j]), std::get<0>(tickets[k])};
+                        }
+                    }        
+                }
+            }
+        }
+    } 
+    
+    if(bestTickets.size() == 0) {
+        // nie znaleziono odpowiedniego biletu lub nie ma biletow
+        return false;
+    }
+    
+    // wypisac bilety z bestTickets;
+    
+    return true;
+}
+
+
 
 void getInput() {
 
@@ -87,7 +212,7 @@ void getInput() {
 }
 
 int main() {
-
+    
     getInput();    
     return 0;
 }
